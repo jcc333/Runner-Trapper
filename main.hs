7bfdfe0bc0c8e9c47185204204012ac83894d570
clem <clemer19@gmail.com>
2013-08-28 23:32:07 -0500

    Serve basic run information with Yesod

--- main.hs
+++ main.hs
@@ -1,10 +1,28 @@
+{-# LANGUAGE TemplateHaskell, 
+  OverloadedStrings,
+  TypeFamilies,
+  QuasiQuotes#-}
 module Main where
 import Distances
 import Runs
 import TimeConversion
+import Yesod
+
+data RunnerTrapper = RunnerTrapper SpeedWork
+
+mkYesod "RunnerTrapper" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod RunnerTrapper
 
 yesterday = SpeedWork [Interval (Miles 1)  6, Interval (Miles 1) 7] [] [Interval (Miles 1) 8]
 
 tomorrow = LongSlowDistance $ Interval (Miles 13) (minutes 80)
 
-main = putStrLn $ show yesterday
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|<p>Yesterday: #{show yesterday}
+                                  <p>Tomorrow: #{show tomorrow}|]
+
+main :: IO ()
+main = warp 3000 $ RunnerTrapper yesterday
